d9c0040042f9a75542f8bc9d2efa754b9a8208ec
Jeremy Placencia Lugo <placenciamjeremy06@gmail.com>
2026-04-25 12:45:31 -0400

    feat: agregar frontend

--- main.py
+++ main.py
@@ -1,11 +1,28 @@
 from fastapi import FastAPI
-from routers import clientes
-from routers import pedidos
-from routers import prendas
-from routers import detalle_pedido
+from fastapi.middleware.cors import CORSMiddleware
+from routers import clientes, pedidos, prendas, detalle_pedido
+from fastapi.staticfiles import StaticFiles
+from fastapi.responses import FileResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.responses import FileResponse
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*", "http://127.0.0.1:5500", "http://localhost:5500"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(clientes.router)
 app.include_router(pedidos.router)
 app.include_router(prendas.router)
 app.include_router(detalle_pedido.router)
+
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+@app.get("/")
+def frontend():
+    return FileResponse("static/index.html")
